5c03f5574f091f1f18ac98fdb1993bf2259ecf08
sagar25bce11092-svg <sagar.25bce11092@vitbhopal.ac.in>
2026-07-30 22:56:07 +0530

    Update security.py

--- Backend/backend/security.py
+++ Backend/backend/security.py
@@ -11,17 +11,14 @@ pwd_context = CryptContext(
     deprecated="auto"
 )
 
-
 def hash_password(password: str):
     return pwd_context.hash(password)
-
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(
         plain_password,
         hashed_password
     )
-
 
 def create_access_token(data: dict):
 
