2b828cf50f65109fee88e891d0074a19f2498ec6
Mayank kumar <mayank.kumar.viiih.41@gmail.com>
2026-07-30 20:41:54 +0530

    Refactor authentication functions in auth.py

--- BACKEND/backend/auth.py
+++ BACKEND/backend/auth.py
@@ -1,56 +1,45 @@
+#importing required libaries
 from sqlalchemy.orm import Session
-
 from . import crud
 from .security import (
     verify_password,
     create_access_token
 )
-
-
+#authentication using eamil,password
 def authenticate_user(
         db: Session,
         email: str,
         password: str
 ):
-
     user = crud.get_user_by_email(
         db,
         email
     )
-
     if not user:
         return None
-
     if not verify_password(
             password,
             user.password
     ):
         return None
-
     return user
-
-
 def login_user(
         db: Session,
         email: str,
         password: str
 ):
-
     user = authenticate_user(
         db,
         email,
         password
     )
-
     if not user:
         return None
-
     token = create_access_token(
         {
             "sub": user.email
         }
     )
-
     return {
         "access_token": token,
         "token_type": "bearer"
