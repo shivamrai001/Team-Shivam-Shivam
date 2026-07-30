0ef4f215a35fa41dc07ed5ad2190cbede82f8e94
Mayank kumar <mayank.kumar.viiih.41@gmail.com>
2026-07-30 20:53:12 +0530

    Update severity.py

--- BACKEND/backend/ai/severity.py
+++ BACKEND/backend/ai/severity.py
@@ -1,24 +1,16 @@
 def calculate_severity(category, description):
-
     score = 20
-
     text = description.lower()
-
     if category == "Road":
         score += 40
-
     elif category == "Garbage":
         score += 30
-
     elif category == "Water":
         score += 35
-
     elif category == "Electricity":
         score += 45
-
     elif category == "Environment":
         score += 25
-
     emergency = [
         "accident",
         "danger",
@@ -27,9 +19,7 @@ def calculate_severity(category, description):
         "school",
         "children"
     ]
-
     for word in emergency:
         if word in text:
             score += 10
-
     return min(score, 100)
