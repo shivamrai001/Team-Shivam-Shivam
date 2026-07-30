1f625b3afc9265512027f2245c4f61e60502bb63
Mayank kumar <mayank.kumar.viiih.41@gmail.com>
2026-07-30 20:46:18 +0530

    Refactor dashboard.py and fix return statement
    Fixed return statement in emergency_complaints function and added missing newline at the end of the file.

--- BACKEND/backend/routers/dashboard.py
+++ BACKEND/backend/routers/dashboard.py
@@ -1,50 +1,33 @@
+#importing
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
 from ..database import get_db
 from .. import crud
-
 router = APIRouter(
     prefix="/dashboard",
     tags=["Government Dashboard"]
 )
-
-
 @router.get("/summary")
+#working on dashboard
 def dashboard_summary(db: Session = Depends(get_db)):
-
     return {
-
         "total_complaints": crud.total_complaints(db),
-
         "pending": crud.pending_complaints(db),
-
         "resolved": crud.resolved_complaints(db),
-
         "rejected": crud.rejected_complaints(db),
-
         "in_progress": crud.inprogress_complaints(db)
-
     }
-
-
 @router.get("/emergency")
 def emergency_complaints(db: Session = Depends(get_db)):
-
     complaints = crud.complaints_by_priority(
         db,
         "Emergency"
     )
-
-    return complaints
-
-
+    return complaint
 @router.get("/critical")
 def critical_complaints(db: Session = Depends(get_db)):
-
     complaints = crud.complaints_by_priority(
         db,
         "Critical"
     )
-
     return complaints
