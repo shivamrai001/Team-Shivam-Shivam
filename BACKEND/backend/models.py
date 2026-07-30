5ad6bc6d5c57ca1da0801ba1c6f8d19165f96991
Mayank kumar <mayank.kumar.viiih.41@gmail.com>
2026-07-30 20:27:34 +0530

    Update models.py

--- BACKEND/backend/models.py
+++ BACKEND/backend/models.py
@@ -2,49 +2,25 @@ from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import Float
-
 from .database import Base
-
-
 class User(Base):
-
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
-
     name = Column(String)
-
     email = Column(String, unique=True)
-
     password = Column(String)
-
-
 class Complaint(Base):
-
     __tablename__ = "complaints"
-
     id = Column(Integer, primary_key=True, index=True)
-
     title = Column(String)
-
     description = Column(String)
-
     image = Column(String)
-
     latitude = Column(Float)
-
     longitude = Column(Float)
-
     category = Column(String)
-
     severity_score = Column(Integer)
-
     priority = Column(String)
-
     genuine_score = Column(Float)
-
     department = Column(String)
-
     status = Column(String)
-
     created_at = Column(String)
