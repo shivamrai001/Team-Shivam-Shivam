0f9d5083f3b2cb5b0a30e5b3709271a40f283011
Mayank kumar <mayank.kumar.viiih.41@gmail.com>
2026-07-30 20:47:46 +0530

    Refactor map_markers function in maps.py

--- BACKEND/backend/routers/maps.py
+++ BACKEND/backend/routers/maps.py
@@ -1,40 +1,25 @@
+#importing libarires
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
 from ..database import get_db
 from .. import crud
-
 router = APIRouter(
     prefix="/maps",
     tags=["Interactive Maps"]
 )
-
-
 @router.get("/markers")
+#working on map
 def map_markers(db: Session = Depends(get_db)):
-
     complaints = crud.get_all_complaints(db)
-
     data = []
-
     for c in complaints:
-
         data.append({
-
             "id": c.id,
-
             "title": c.title,
-
             "category": c.category,
-
             "priority": c.priority,
-
             "latitude": c.latitude,
-
             "longitude": c.longitude,
-
             "status": c.status
-
         })
-
     return data
